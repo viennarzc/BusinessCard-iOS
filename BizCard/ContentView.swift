00305d14127bf3bcb0f3124820568dcb5ae92f5d
Viennarz Curtiz <viennarzc@gmail.com>
2020-09-17 13:39:59 +0800

    Initial Commit

---
+++ BizCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BizCard
+//
+//  Created by Viennarz Curtiz on 9/17/20.
+//  Copyright © 2020 Viennarz Curtiz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
